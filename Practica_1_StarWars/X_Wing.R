19a5525d3fa4aff623e15ac8f41330428d82e694
armin <armingarc@yahoo.com>
2026-02-22 21:33:45 +0100

    Created main file structure for the StarWars project

---
+++ Practica_1_StarWars/X_Wing.R
@@ -0,0 +1,39 @@
+##################################################################################
+# Práctica 1: Nave de Star Wars
+# Autores: Rodrigo López Torres y Arminda García Moreno
+# Fecha : 22/02/2026 
+# Propósito: Calcular el área de una nave de Star Wars descomprimiéndola en triángulos
+##################################################################################
+
+# ---- PROCESS FIGURE FUNCTIONS ----
+process_triangle <- function(vector){
+  
+}
+
+process_paralelogram <- function(vector){
+  
+}
+
+# ---- AREA CALCULATION FUNCTIONS ----
+area_triangle <- function(p1,p2,p3){
+  
+}
+
+area_paralelogram <- function(p1,p2,p3,p4){
+  
+}
+
+# ---- DRAW FIGURES FUNCTIONS ----
+draw_triangle <- function(p1,p2,p3){
+  
+}
+
+draw_paralelogram <- function(p1,p2,p3,p4){
+  
+}
+
+# ---- MAIN FUNCTION ----
+area_spaceship <- function(path){ 
+  # path = path to the xlsx file containing the spaceship reference points
+
+}
