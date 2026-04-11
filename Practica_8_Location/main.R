a27a97c844897148e2249548fc8771d95a7b13b9
armin <armingarc@yahoo.com>
2026-04-11 19:03:53 +0200

    Add purpose on project 8

--- Practica_8_Location/main.R
+++ Practica_8_Location/main.R
@@ -2,7 +2,7 @@
 # Práctica 8: Localización
 # Autores: Rodrigo López Torres y Arminda García Moreno
 # Fecha : 26/03/2026 
-# Propósito: 
+# Propósito: realizar un algoritmo greedy para resolver el problema de la p-mediana
 ##############################################################################
 
 # Data Structs
@@ -17,7 +17,7 @@ node <- function(name, coordinates, demand){
     demand = demand, 
     server = NULL, 
     used_capacity = 0, 
-    score = 0
+    score = Inf
   )
 }
 
@@ -160,57 +160,56 @@ greedy_establish_servers <- function(nodes, p, limit_capacity){
     candidates <- remove_node_by_name(candidates, potential_server$name)
     
     # Server attend itself
-    potential_server$used_capacity <- potential_server$demand 
     server_idx <- find_node_index_by_name(nodes, potential_server$name)
     nodes[[server_idx]]$server <- potential_server$name
+    potential_server$used_capacity <- potential_server$demand
     
     # Assign nearest clients while capacity allows
     client_queue <- get_client_queue(potential_server, candidates)
-    i <- 1
-    while (i <= length(client_queue)) {
-      client <- client_queue[[i]]
-      
+    for (client in client_queue) {
       if (potential_server$used_capacity + client$demand <= limit_capacity) {
         potential_server$used_capacity <- potential_server$used_capacity + client$demand
+        
         client_idx <- find_node_index_by_name(nodes, client$name)
         nodes[[client_idx]]$server <- potential_server$name
+        
         candidates <- remove_node_by_name(candidates, client$name)
       }
-      
-      i <- i + 1
     }
     
     # Add server to partial solution
+    nodes[[server_idx]]$used_capacity <- potential_server$used_capacity
     servers[[length(servers) + 1]] <- potential_server
   }
   
-  if (length(servers) == p && length(candidates) == 0){
+  unassigned <- sapply(nodes, function(nd) is.null(nd$server))
+  
+  if (all(!unassigned)) {
     return(list(nodes = nodes, servers = servers))
   } else {
-    stop("Unreacheable solution with this greedy strategy")
+    stop("Greedy heuristic failed to build a complete feasible solution")
   }
 }
 
-# Evaluate optimal solution
-objective_function <- function(solution){
+# Objective function
+objective_function <- function(solution) {
   total_value <- 0
   
-  for (server in solution$servers){
-    for (node in solution$nodes){
-      if (node$server == server$name){
-        total_value <- total_value + node$demand * 
+  for (server in solution$servers) {
+    for (node in solution$nodes) {
+      if (!is.null(node$server) && node$server == server$name) {
+        total_value <- total_value +
           euclidean_distance(server$coordinates, node$coordinates)
       }
     }
   }
   
-  return(total_value)
+  total_value
 }
 
 evaluate_solution_quality <- function(solution, best_value){
-  data_objective_function <- objective_function(solution)
-  min_data <- min(data_objective_function)
-  gap <- (min_data - best_value) / best_value * 100
+  data <- objective_function(solution)
+  gap <- (data - best_value) / best_value * 100
   gap
 }
 
@@ -266,25 +265,34 @@ plot_solution_connections <- function(solution, alpha) {
 # Testing 1
 data <- create_nodes_from_file("Practica_8_Location/phub_50_5_1.txt")
 solution <- greedy_establish_servers(data$nodes, data$p, data$capacity)
-evaluate_solution_quality(solution,713)
+objective_function(solution)
+evaluate_solution_quality(solution, 713)
 plot_solution_connections(solution,10)
 
 # Testing 2
 data <- create_nodes_from_file("Practica_8_Location/phub_50_5_2.txt")
 solution <- greedy_establish_servers(data$nodes, data$p, data$capacity)
+objective_function(solution)
+evaluate_solution_quality(solution, 740)
 plot_solution_connections(solution,8)
 
 # Testing 3
 data <- create_nodes_from_file("Practica_8_Location/phub_50_5_3.txt")
 solution <- greedy_establish_servers(data$nodes, data$p, data$capacity)
+objective_function(solution)
+evaluate_solution_quality(solution, 751)
 plot_solution_connections(solution,7)
 
 # Testing 4
 data <- create_nodes_from_file("Practica_8_Location/phub_50_5_4.txt")
 solution <- greedy_establish_servers(data$nodes, data$p, data$capacity)
+objective_function(solution)
+evaluate_solution_quality(solution, 651)
 plot_solution_connections(solution,7)
 
 # Testing 5
 data <- create_nodes_from_file("Practica_8_Location/phub_50_5_5.txt")
 solution <- greedy_establish_servers(data$nodes, data$p, data$capacity)
+objective_function(solution)
+evaluate_solution_quality(solution, 664)
 plot_solution_connections(solution,10)
